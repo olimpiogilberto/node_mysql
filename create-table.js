5241e6963de9910c26d81f5ec87961fa9ed81c73
olimpiogilberto <olimpiogilberto@hotmail.com>
2020-08-15 17:33:41 -0300

    foi capaz criar tabela

--- create-table.js
+++ create-table.js
@@ -4,6 +4,37 @@ const connection = mysql.createConnection({
     port : '3306',
     user : 'user_node',
     password : 'admin',
-    database : 'xxx'
+    database : 'node_mysql'
 
 });
+
+function createTable(conn){
+ 
+    const sql = "CREATE TABLE IF NOT EXISTS Clientes ( ID int NOT NULL AUTO_INCREMENT,Nome varchar(150) NOT NULL,"+
+                "CPF char(11) NOT NULL, PRIMARY KEY (ID) );";
+    
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Criou a tabela!');
+    });
+}
+
+connection.connect(function(err){
+  if(err) return console.log(err);
+  console.log('Banco conectado!!');
+  createTable(connection);
+})
+
+function addRows(conn){
+//   const sql = "INSERT INTO Clientes(Nome,CPF) VALUES ?";
+//   const values = [
+//         ['Giba', '32672467846'],
+//         ['Vini', '41885192827'],
+//         ['teste3', '12312312399']
+//       ];
+  conn.query(sql, [values], function (error, results, fields){
+          if(error) return console.log(error);
+          console.log('Clientes cadastrados!!');
+          conn.end();//fecha a conexão
+      });
+}
